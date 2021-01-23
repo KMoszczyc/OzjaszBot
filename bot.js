2ec6236f6f625e564026af923ef8011dc18d9715
Kamil Moszczyc <likeyouop@gmail.com>
2021-01-23 14:36:31 +0100

    Fix lagginess in ytdl.

--- bot.js
+++ bot.js
@@ -336,7 +336,7 @@ async function play(guild, song) {
     }
 
     const dispatcher = serverQueue.connection
-        .play(ytdl(song.url))
+        .play(ytdl(song.url, {filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1<<25 }), {highWaterMark: 1})
         .on('finish', () => {
             serverQueue.songs.shift()
             play(guild, serverQueue.songs[0])
